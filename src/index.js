51c4428ebbf2bf31cab5abbd54c2b4524846fb22
toaingo2108 <nqtoai2181@gmail.com>
2021-11-02 21:58:22 +0700

    fix errors

--- src/index.js
+++ src/index.js
@@ -40,5 +40,5 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 route(app);
 
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}/invoices`);
+    console.log(`App listening at http://localhost:${port}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,9 @@ const invoicesRouter = require('./invoice');
 function route(app) {
     app.use('/invoices', invoicesRouter);
 
+    app.use('/', (req, res) => {
+        res.render('home')
+    })
 }
 
 module.exports = route;
