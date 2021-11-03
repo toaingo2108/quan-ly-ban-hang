e63390131220bfae171b2c038a5085453c03c664
toaingo2108 <nqtoai2181@gmail.com>
2021-11-03 21:57:53 +0700

    adding script for heroku

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const handlebars = require('express-handlebars');
 const route = require('./routes/index');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
